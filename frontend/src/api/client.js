44ae6b881a829a1b15bd12468e2a3c2b426969fc
Isabel <isabel14myg@gmail.com>
2026-07-15 10:32:19 -0500

    cambio

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  baseURL: "https://trabajo-final-daw.onrender.com/api",
 });
 
 client.interceptors.request.use((config) => {
